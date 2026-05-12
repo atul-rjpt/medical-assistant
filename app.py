843875971cb3c94e06322f2e4058ec3a49e813ca
atul-rjpt <a.rjpt@outlook.in>
2026-05-12 12:08:24 +0530

    fix text splitters import

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
